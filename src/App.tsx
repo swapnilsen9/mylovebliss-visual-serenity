d705adb60528d38fc7d525fe77093abc6c16199b
swapnilsen9 <swapnilsen451@gmail.com>
2025-06-25 04:48:15 +0530

    Updated for lovable

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/mylovebliss-visual-serenity/">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/product/:productId" element={<ProductDetail />} />
